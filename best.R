d1490994b1899e64fd96259164b06e7aecd8355f
aidoskanapyanov <aidos.kanapyanov@nu.edu.kz>
2020-05-24 00:16:30 +0600

    Rearranged files, finished part 1

--- best.R
+++ best.R
@@ -10,6 +10,27 @@ best <- function(state, outcome) {
     if (sum(c("heart attack","heart failure","pneumonia")==outcome)<1) {
         stop("invalid outcome")
     }
+    
     ## Return hospital name in that state with lowest 30-day death
     ## rate
+    if (outcome == "heart attack") {
+        # lowest heart attack mortality
+        min_heart = min(as.numeric(data[data[,7]==state,11]), na.rm = TRUE)
+        ret = levels(factor(data[data[,7]==state,][as.numeric(data[data[,7]==state,11])==min_heart,2]))[1]
+        return(ret)
+    }
+    
+    if (outcome == "heart failure") {
+        # lowest heart failure mortality
+        min_heart = min(as.numeric(data[data[,7]==state,17]), na.rm = TRUE)
+        ret = levels(factor(data[data[,7]==state,][as.numeric(data[data[,7]==state,17])==min_heart,2]))[1]
+        return(ret)
+    }
+    
+    if (outcome == "pneumonia") {
+        # lowest pneumonia mortality
+        min_heart = min(as.numeric(data[data[,7]==state,23]), na.rm = TRUE)
+        ret = levels(factor(data[data[,7]==state,][as.numeric(data[data[,7]==state,23])==min_heart,2]))[1]
+        return(ret)
+    }
 }
--- main.R
+++ main.R
@@ -4,3 +4,13 @@ head(outcome)
 outcome[, 11] <- as.numeric(outcome[, 11])
 ## You may get a warning about NAs being introduced; that is okay
 hist(outcome[, 11])
+
+data_new = outcome[,c(2,7,11)]
+
+source("best.R")
+best("TX", "heart attack")
+best("TX", "heart failure")
+best("MD", "heart attack")
+best("MD", "pneumonia")
+best("BB", "heart attack")
+best("NY", "hert attack")
